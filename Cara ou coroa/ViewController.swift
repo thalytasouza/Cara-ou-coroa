0aab4d00cba13bdc81c7d64f16418797e43a1824
Thalyta Souza <mayerflot@gmail.com>
2023-11-18 13:44:52 -0300

    Initial Commit

---
+++ Cara ou coroa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cara ou coroa
+//
+//  Created by Thalytinha on 18/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
