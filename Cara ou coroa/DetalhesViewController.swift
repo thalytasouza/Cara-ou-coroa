900cc584bf00e186b2aa7e595248c4f3de5388e6
Thalyta Souza <mayerflot@gmail.com>
2023-11-19 01:12:20 -0300

    first commit

---
+++ Cara ou coroa/DetalhesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetalhesViewController.swift
+//  Cara ou coroa
+//
+//  Created by Thalytinha on 18/11/23.
+//
+
+import UIKit
+
+class DetalhesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
